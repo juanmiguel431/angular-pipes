755a79dfe8f18c257ce6b8f84626251b43266881
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-28 19:26:11 -0400

    Replace placeholder content with temperature dashboard displaying current and historic temperatures, styled with a new design.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('pipes');
+  currentDate = new Date();
+  currentTemperatures = {
+    berlin: 4.2749812,
+    newYork: 18.1214,
+    paris: 72.1209001,
+    chicago: 65.0775238,
+  };
+
+  historicTemperatures = [25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5];
+
+  onReset(index: number) {
+    this.historicTemperatures[index] = 18;
+  }
 }
